7629da324bf21079e001ecf72674c79b36706227
Krishna Parmar <63655430+ManavKanpariya@users.noreply.github.com>
2022-08-08 10:02:47 +0530

    changes

--- lib/data/models/message.dart
+++ lib/data/models/message.dart
@@ -17,6 +17,7 @@ class Message extends Equatable {
   bool isThatImage;
   bool isThatPost;
   bool multiImages;
+  bool isThatVideo;
   String postId;
   String profileImageUrl;
   String userNameOfSharedPost;
@@ -28,6 +29,7 @@ class Message extends Equatable {
     required this.receiverId,
     this.isThatPost = false,
     required this.senderId,
+    this.isThatVideo = false,
     this.postId = "",
     this.profileImageUrl = "",
     this.userNameOfSharedPost = "",
@@ -54,6 +56,7 @@ class Message extends Equatable {
       profileImageUrl: snap.data()["profileImageUrl"] ?? "",
       userNameOfSharedPost: snap.data()["userNameOfSharedPost"] ?? "",
       multiImages: snap.data()["multiImages"] ?? false,
+      isThatVideo: snap.data()["isThatVideo"] ?? false,
     );
   }
 
@@ -71,6 +74,7 @@ class Message extends Equatable {
         "profileImageUrl": profileImageUrl,
         "userNameOfSharedPost": userNameOfSharedPost,
         "multiImages": multiImages,
+        "isThatVideo": isThatVideo,
       };
 
   @override
@@ -89,5 +93,6 @@ class Message extends Equatable {
         profileImageUrl,
         userNameOfSharedPost,
         multiImages,
+        isThatVideo,
       ];
 }
